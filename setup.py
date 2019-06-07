b07ba1de4b0da8a98c149131365139cabaa03814
Pål Grønås Drange <PGDR@statoil.com>
2019-06-07 19:30:36 +0200

    Add requires in setup

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
+import os
 import setuptools
+
+
+def src(x):
+    root = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(root, x))
+
+
+def _read_file(fname, op):
+    with open(src(fname), "r") as fin:
+        return op(fin.readlines())
+
+
+def requirements():
+    return _read_file("requirements.txt", lambda lines: "".join(lines))
+
 
 setuptools.setup(
     name="snake",
@@ -7,4 +23,5 @@ setuptools.setup(
     version="1.0",
     author="pgdr",
     license="MIT",
+    install_requires=requirements(),
 )
