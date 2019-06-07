160daf33ff58b2cfdebb882a8020d820378b3b81
Pål Grønås Drange <PGDR@statoil.com>
2019-06-07 20:18:39 +0200

    De-OOP

--- snake/__init__.py
+++ snake/__init__.py
@@ -9,54 +9,55 @@ APPLE_COLOR = (100, 255, 80)
 INITIAL_SNAKE_SIZE = 7
 
 
-class Snake(list):
-    def next_snake(self, direction, apples):
-        tail = self[-1]
-        head = self[0]
-        next_head = (
-            (head[0] + direction[0]) % (size[0] // block),
-            (head[1] + direction[1]) % (size[1] // block),
+def next_snake(snake, direction, apples):
+    tail = snake[-1]
+    head = snake[0]
+    next_head = (
+        (head[0] + direction[0]) % (size[0] // block),
+        (head[1] + direction[1]) % (size[1] // block),
+    )
+
+    if next_head in snake:
+        score = len(snake) - INITIAL_SNAKE_SIZE
+        print("You eat yourself.\nScore: {}".format(score))
+        pygame.quit()
+        exit()
+
+    if next_head in apples:
+        apples.remove(next_head)
+        return [next_head] + snake
+    return [next_head] + snake[:-1]
+
+
+def draw_snake(snake, surface):
+    for s in snake:
+        pygame.draw.rect(
+            surface, SNAKE_COLOR, (s[0] * block, s[1] * block, block, block)
         )
 
-        if next_head in self:
-            score = len(self) - INITIAL_SNAKE_SIZE
-            print("You eat yourself.\nScore: {}".format(score))
-            pygame.quit()
-            exit()
 
-        if next_head in apples:
-            apples.remove(next_head)
-            return Snake([next_head] + self)
-        return Snake([next_head] + self[:-1])
-
-    def draw(self, surface):
-        for s in self:
-            pygame.draw.rect(
-                surface, SNAKE_COLOR, (s[0] * block, s[1] * block, block, block)
-            )
+def draw_apples(apples, surface):
+    for apple in apples:
+        pygame.draw.rect(
+            surface, APPLE_COLOR, (apple[0] * block, apple[1] * block, block, block)
+        )
 
 
-class Apples(list):
-    def draw(self, surface):
-        for apple in self:
-            pygame.draw.rect(
-                surface, APPLE_COLOR, (apple[0] * block, apple[1] * block, block, block)
-            )
+def generate(apples):
+    if len(apples) >= 4:
+        return
+    if random.random() > 0.1:
+        return
+    x = random.randint(1, (size[0] - block) // block)
+    y = random.randint(1, (size[1] - block) // block)
+    apples.append((x, y))
 
-    def generate(self):
-        if len(self) >= 4:
-            return
-        if random.random() > 0.1:
-            return
-        x = random.randint(1, (size[0] - block) // block)
-        y = random.randint(1, (size[1] - block) // block)
-        self.append((x, y))
 
-    def degenerate(self):
-        if len(self) <= 3:
-            return
-        if random.random() < 0.01:
-            self.remove(self[0])
+def degenerate(apples):
+    if len(apples) <= 3:
+        return
+    if random.random() < 0.01:
+        apples.remove(apples[0])
 
 
 def get_dir(key, direction):
@@ -93,42 +94,31 @@ def handle_event(event, direction):
                 return d
 
 
-def main():
+def game_loop(snake, apples, srfc, clock):
     global SNAKE_COLOR
-
-    pygame.init()
-    pygame.font.init()
-    myfont = pygame.font.SysFont("DejaVu", 42)
-    pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
-    srfc = pygame.display.get_surface()
-
-    snake = Snake([(i, 17) for i in range(10, 10 - INITIAL_SNAKE_SIZE, -1)])
     direction = (1, 0)
-    apples = Apples([(5, 5)])
-
+    myfont = pygame.font.SysFont("DejaVu", 42)
     scores = {
         score: myfont.render(
             "Score: {}".format(score - INITIAL_SNAKE_SIZE), False, (255, 255, 0)
         )
         for score in range(50)
     }
-
     while True:
-        apples.generate()
-        apples.degenerate()
+        generate(apples)
+        degenerate(apples)
 
         srfc.fill((0, 0, 0))
 
-        apples.draw(srfc)
+        draw_apples(apples, srfc)
 
         for event in list(pygame.event.get()):
             nd = handle_event(event, direction)
             if nd:
                 direction = nd
             break
-        snake = snake.next_snake(direction, apples)
-        snake.draw(srfc)
+        snake = next_snake(snake, direction, apples)
+        draw_snake(snake, srfc)
 
         score = scores[len(snake)]
         srfc.blit(score, (800, 20))
@@ -136,6 +126,20 @@ def main():
         r = min(255, 100 + len(snake) * 5)
         SNAKE_COLOR = r, 255 - r, 255 - r
         pygame.display.flip()
+
+
+def main():
+    pygame.init()
+    pygame.font.init()
+
+    pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    srfc = pygame.display.get_surface()
+
+    snake = [(i, 17) for i in range(10, 10 - INITIAL_SNAKE_SIZE, -1)]
+    apples = [(5, 5)]
+
+    game_loop(snake, apples, srfc, clock)
 
 
 if __name__ == "__main__":
